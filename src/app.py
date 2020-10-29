ff7f644f1b73aec2f84c901326c98b128df778f5
jgustavoj <gjime011@fiu.edu>
2020-10-29 01:01:11 +0000

    5

--- src/app.py
+++ src/app.py
@@ -1,22 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
 
 
 
